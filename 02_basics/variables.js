031acda908016621903d9998b3ee80bda31c48d1
ShamlaZunjare <153799086+ShamlaZunjare@users.noreply.github.com>
2024-02-04 14:12:34 +0000

    variables.js file added

---
+++ 02_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId=123654;
+let accountEmail="sz@sz.com";
+var accountPassowrd="12457";
+accountCity="mumbai";
+let accountState;
+// accountId=789456;
+// console.log(accounId);
+
+ accountEmail="hc@sz.com";
+ accountPassowrd="1245678";
+accountCity="bangaluru";
+
+
+console.table([accountId,accountEmail,accountPassowrd,accountCity,accountState]);
